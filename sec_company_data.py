b14a7728d72dcba5445add99c9db4e18de877bc6
Chakravarthy V <chakravarthyv997@gmail.com>
2021-07-12 21:03:05 +0100

    account stats function added and cleaned up rest of code

--- sec_company_data.py
+++ sec_company_data.py
@@ -1,9 +1,7 @@
 import json
 import requests
 import pandas as pd
-import numpy as np
-
-#pd.options.mode.chained_assignment = None  # default='warn'
+import matplotlib.pyplot as plt
 
 """
 SEC SERVER LIMIT PROBLEM
@@ -15,10 +13,10 @@ company_data = requests.get(base_url.format(cik)).json()
 company_file = open('CIK0001326380.json',)
 account_name = input('Enter account name: ')
 
-def account_tracker(account):
+def account_storage(account):
     """
     For a chosen account (e.g., amortization of intangible assets), the function
-    converts (from .json to pandas dataframe), cleans, and tracks its historical data.
+    converts (from .json to pandas df), cleans, and stores its historical data.
     """
     
     data = json.load(company_file)
@@ -37,13 +35,31 @@ def account_tracker(account):
         account_data = account_data.append(df1) #append remaining two columns to the initially empty dataframe 
     
     account_data = account_data.dropna() #remove repeated values (e.g., Q4 data = 10k data)
-    account_data = account_data.rename(columns={"val": "value", "frame": "date"})
+    account_data = account_data.rename(columns={"val": "Amount ($)", "frame": "Year"}) #change column names
     account_data = account_data.reset_index(drop=True) #label rows numerically
-    account_data['date'] = account_data['date'].map(lambda x: x.lstrip('cCyY').rstrip('iI')) #remove substrings in column
+    account_data['Year'] = account_data['Year'].map(lambda x: x.lstrip('cCyY').rstrip('iI')) #remove substrings in column
     
-    return account_data
+    return account_data #quarterly data
 
-print(account_tracker(account_name))
+def account_stats():
+    """
+    Presents YoY change in account data (as a bar graph). 
+    """
+    
+    account = account_storage(account_name) #quarterly df from account_storage function
+    quarters = ['1', '2', '3', '4']
+    quarter_choice = input('Quarter of choice: ')
+    
+    for quarter in quarters:
+        if quarter != quarter_choice:
+            account = account[~account.Year.str.contains('Q' + quarter)] #removes e.g. Q1/2/3 data if Q4 chosen
+    account['Year'] = account['Year'].str[2:] #remove 20---- from e.g. 2008Q2
+    
+    ax = account.plot(kind='bar', title=account_name, x='Year', y='Amount ($)')
+    
+    return ax
+
+print(account_stats())
 
 
 
