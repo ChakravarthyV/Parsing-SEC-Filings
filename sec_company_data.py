ffd78baf0b57a92ac9fc119a6533946e719649eb
Chakravarthy V <chakravarthyv997@gmail.com>
2021-10-28 18:02:09 +0100

    comments added; code cleaned up

--- sec_company_data.py
+++ sec_company_data.py
@@ -34,7 +34,8 @@ def json_to_pd(account):
     return account_data #returns quarterly data as a pandas df
 
 #global variables [parses through the recent quarter report]
-cash = json_to_pd('CashAndCashEquivalentAtCarryingValue')['CashAndCashEquivalentAtCarryingValue'].iloc[-1]
+print(" --- Definitions of Used XBRL Accounts --- ")
+cash = json_to_pd('CashAndCashEquivalentsAtCarryingValue')['CashAndCashEquivalentsAtCarryingValue'].iloc[-1]
 receivables = json_to_pd('AccountsReceivableNetCurrent')['AccountsReceivableNetCurrent'].iloc[-1]
 inventories = json_to_pd('InventoryNet')['InventoryNet'].iloc[-1]
 current_prepaid = json_to_pd('OtherPrepaidExpenseCurrent')['OtherPrepaidExpenseCurrent'].iloc[-1]
@@ -47,31 +48,40 @@ goodwill = json_to_pd('Goodwill')['Goodwill'].iloc[-1]
 
 total_liabilities = json_to_pd('Liabilities')['Liabilities'].iloc[-1] #taken at face value
 
-def liquidation_value():
+def net_net():
     """
-    Assumes liquidation is immediate (< 3 years). Returns a conservative estimate
-    that should, in theory, provide a floor to a firm's fundamental value. In most cases, it will be
-    negative since it assumes a worst case scenario where assets reported are difficult to sell and 
-    all the liabilities are taken at face value.
+    Returns a conservative estimate that should, in theory, provide a floor to a firm's fundamental value. For most companies,
+    its net-net will amount to a negative value. The drawbacks to this approach are listed in detail on the read.me file.
     """    
-    receivables_writedown = receivables*0.15 #takes the last element of receivables column and gives it a 15% writedown
-    inventories_writedown = inventories*0.50 #50% reduction
+    net_net_value = current_assets - total_liabilities
     
-    adj_current_assets = current_assets - (receivables_writedown + inventories_writedown) #current assets adjusted for conservative writedowns
-    
-    adj_ppe = ppe*0.45 #reported ppe discounted by 45%
-    
-    adj_total_assets = adj_current_assets + adj_ppe
-    
-    liq_val = adj_total_assets - total_liabilities
-    
-    print(liq_val)
+    return "Net-Net Working Capital: " + str(net_net_value)
 
 def adjusted_nav():
     """
-    Allows the user to adjust reported accounts as he/she sees fit. The user-adjusted net asset value
-    of the company is returned.
+    Allows the user to adjust assets as they see fit and all reported liabilities are taken at face value. 
+    The user-adjusted net asset value of the company is returned.
     """
+    print("Enter, for each account, your estimated writedown %: \n 1. Receivables \n 2. Inventories \n 3. PPE \n")
     
-print(json_to_pd('AssetsCurrent'))
-print(liquidation_value())
+    p_list = []
+    
+    for x in range(3):
+        y = float(input('%: '))
+        p_list.append(y)
+        
+    receivables_writedown = receivables*(1 - p_list[0]/100) #takes the last element of receivables column and gives it a 15% writedown
+    inventories_writedown = inventories*(1 - p_list[1]/100) #50% reduction
+    
+    adj_current_assets = current_assets - (receivables_writedown + inventories_writedown) #current assets adjusted for conservative writedowns
+    
+    adj_ppe = ppe*(1 - p_list[2]/100) #reported ppe discounted by 45%
+    
+    adj_total_assets = adj_current_assets + adj_ppe
+    
+    adj_nav = adj_total_assets - total_liabilities
+    
+    return "Adjusted Net Asset Value (NAV): " + str(adj_nav)
+
+print(net_net())
+print(adjusted_nav())
